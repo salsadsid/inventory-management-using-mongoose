3233014983986053a2d6c052a9bb3e81f1bbbb1d
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2022-10-10 16:07:31 +0600

    user-signup-login-persistence-jwt-implemented

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const storeRoute = require('./routes/store.route')
 const categoryRoute = require('./routes/category.route')
 const supplierRoute = require('./routes/supplier.route')
 const stockRoute = require('./routes/stock.route')
+const userRoute = require('./routes/user.route')
 
 app.use(express.json());
 app.use(cors());
@@ -20,5 +21,6 @@ app.use("/api/v1/store", storeRoute)
 app.use("/api/v1/category", categoryRoute)
 app.use("/api/v1/supplier", supplierRoute)
 app.use("/api/v1/stock", stockRoute)
+app.use("/api/v1/user", userRoute)
 
 module.exports = app;
